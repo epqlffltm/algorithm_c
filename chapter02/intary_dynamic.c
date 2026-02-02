650445e620d3e03077cd79532745fa0767c7c298
epqlffltm <kjh55984249@gmail.com>
2026-02-02 16:18:54 +0900

    동적 배열 생성

---
+++ chapter02/intary_dynamic.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+
+int main(void)
+{
+  srand((unsigned)time(NULL));
+
+  int n = 0;
+
+  printf("숫자를 입력하세요: ");
+
+  if(scanf("%d", &n) < 1 || n <= 0) 
+  {
+    perror("perror로 출력"); 
+    exit(EXIT_FAILURE);
+  }
+
+  int *p = (int*)malloc(sizeof(int)*n);
+  if(p == NULL) 
+  {
+    perror("perror로 출력"); 
+    exit(EXIT_FAILURE);
+  }
+
+  for(int i=0; i<n; i++)
+  {
+    p[i] = rand()%100 + 10;
+  }
+
+  printf("%d개의 난수\n", n);
+  for(int i=0; i<n; i++)
+  {
+    printf("%d ", *(p + i));
+  }
+  free(p);
+  
+  return 0;
+}
