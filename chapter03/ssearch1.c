dc7efafa6cda8070b29e1f23e84e94d80aa11e1f
epqlffltm <kjh55984249@gmail.com>
2026-02-09 19:58:27 +0900

    ssearch1

--- chapter03/ssearch1.c
+++ chapter03/ssearch1.c
@@ -5,9 +5,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<stdbool>
+#include<stdbool.h>
 
-int search(const int a[], int n, int key);
+int search(const int ary[],  int* const n,  int* const key);
 
 int main(void)
 {
@@ -17,7 +17,7 @@ int main(void)
   puts("선형 검색");
   printf("요소 개수: %d\n",nx);
   
-  int *ary = (int*)malloc(nx, sizeof(int));
+  int *ary = (int*)malloc(nx * sizeof(int));
   
   for(int i = 0; i < nx; i++)
   {
@@ -26,7 +26,7 @@ int main(void)
 
   printf("검색값%d\n",ky);
 
-  int idx = search(ary, nx, ky);
+  int idx = search(ary, &nx, &ky);
 
   idx == -1 ? printf("검색 실패\n") : printf("%d -> ary[%d]\n",ky, idx );
   
@@ -35,12 +35,16 @@ int main(void)
   return 0;
 }
 
-int search(const int a[], int n, int key)
+int search(const int ary[],  int* const n,  int* const key)
 {
   int i = 0;
+
   while(true)
   {
-    i == n ? return -1 : return i;
+    if(i == *n)
+    return -1;
+    if(ary[i] == *key)
+    return i;
     i++;
   }
 }
